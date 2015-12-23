c9f962a4457854240719272bbc21aef78bee6ff8
Dominic Meiser <dmeiser@txcorp.com>
2015-12-23 15:38:42 -0500

    Add script to download and build mpich.

---
+++ utilities/get_mpich.sh
@@ -0,0 +1,31 @@
+#!/bin/sh
+
+if [ -e mpich-3.2 ]; then
+  echo "mpich sources found -- don't need to download"
+else
+  echo "Downloading mpich source."
+  wget http://www.mpich.org/static/downloads/3.2/mpich-3.2.tar.gz
+  tar xfz mpich-3.2.tar.gz
+  rm mpich-3.2.tar.gz
+fi
+
+if [ -f mpich/lib/libmpich.so ]; then
+  echo "libmpich.so found -- nothing  to build."
+else
+  echo "configuring and building mpich."
+  cd mpich-3.2
+  ./configure \
+          --prefix=`pwd`/../mpich \
+          --enable-static=false \
+          --enable-alloca=true \
+          --disable-long-double \
+          --enable-threads=single \
+          --enable-fortran=no \
+          --enable-fast=all \
+          --enable-g=none \
+          --enable-timing=none
+  make -j4
+  make install
+  cd -
+fi
+
