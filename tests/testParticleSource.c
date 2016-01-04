aa4863b516edf224238b246c9429828cf37197a5
Dominic Meiser <dmeiser@txcorp.com>
2016-01-04 10:46:32 -0500

    Test composition of particle sources.

--- tests/testParticleSource.c
+++ tests/testParticleSource.c
@@ -11,14 +11,14 @@ static double vy[ENSEMBLE_CAPACITY];
 static double vz[ENSEMBLE_CAPACITY];
 static double internalState[ENSEMBLE_CAPACITY * INTERNAL_STATE_SIZE];
 static struct BBox volume = {0.0, 1.0, -1.0, 0.5, -2.5, 3.0};
-static struct ParticleSource *particleSource;
+static double vbar[3] = {0.0, 0.0, -200.0};
+static double deltaV[3] = {1.0, 1.0, 10.0};
+static double initialState[4] = {0, 0, 1.0, 0};
 static const int numPtcls = 5;
+static struct ParticleSource *particleSource;
 
 Describe(ParticleSource)
 BeforeEach(ParticleSource) {
-  double vbar[3] = {0.0, 0.0, -200.0};
-  double deltaV[3] = {1.0, 1.0, 10.0};
-  double initialState[4] = {0, 0, 1.0, 0};
   particleSource = blParticleSourceUniformCreate(
       volume, numPtcls, vbar, deltaV, INTERNAL_STATE_SIZE, initialState, 0);
 }
@@ -30,6 +30,12 @@ AfterEach(ParticleSource) {
 Ensure(ParticleSource, nullSourceCreatesZeroParticles) {
   int n = blParticleSourceGetNumParticles(0);
   assert_that(n, is_equal_to(0));
+}
+
+Ensure(ParticleSource, twoSourcesCanBeDestroyed) {
+  particleSource = blParticleSourceUniformCreate(
+      volume, numPtcls, vbar, deltaV, INTERNAL_STATE_SIZE, initialState,
+      particleSource);
 }
 
 Ensure(ParticleSource, uniformSourceProducesNBarParticles) {
@@ -53,12 +59,31 @@ Ensure(ParticleSource, uniformSourceCreatesParticlesInBox) {
   }
 }
 
+Ensure(ParticleSource, twoSourcesCanBeComposed) {
+  particleSource = blParticleSourceUniformCreate(
+      volume, numPtcls, vbar, deltaV, INTERNAL_STATE_SIZE, initialState,
+      particleSource);
+  int n = blParticleSourceGetNumParticles(particleSource);
+  assert_that(n, is_equal_to(2 * numPtcls));
+  int i;
+  for (i = 0; i < 2 * numPtcls; ++i) {
+    assert_that_double(x[i], is_greater_than_double(volume.xmin - 1.0e-6));
+    assert_that_double(x[i], is_less_than_double(volume.xmax + 1.0e-6));
+    assert_that_double(y[i], is_greater_than_double(volume.ymin - 1.0e-6));
+    assert_that_double(y[i], is_less_than_double(volume.ymax + 1.0e-6));
+    assert_that_double(z[i], is_greater_than_double(volume.zmin - 1.0e-6));
+    assert_that_double(z[i], is_less_than_double(volume.zmax + 1.0e-6));
+  }
+}
+
 int main()
 {
   TestSuite *suite = create_test_suite();
   add_test_with_context(suite, ParticleSource, nullSourceCreatesZeroParticles);
+  add_test_with_context(suite, ParticleSource, twoSourcesCanBeDestroyed);
   add_test_with_context(suite, ParticleSource, uniformSourceProducesNBarParticles);
   add_test_with_context(suite, ParticleSource, uniformSourceCreatesParticlesInBox);
+  add_test_with_context(suite, ParticleSource, twoSourcesCanBeComposed);
   int result = run_test_suite(suite, create_text_reporter());
   destroy_test_suite(suite);
   return result;
