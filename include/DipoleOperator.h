90d442be4de1d511b22855b78560a3396eeb57cf
Dominic Meiser <dmeiser@txcorp.com>
2016-01-06 21:12:02 -0700

    Draft interface for dipole operators.

--- include/DipoleOperator.h
+++ include/DipoleOperator.h
@@ -1,5 +1,19 @@
 #ifndef DIPOLE_OPERATOR_H
 #define DIPOLE_OPERATOR_H
 
+struct DipoleOperator {
+  void (*apply)(int stride, int numPtcls,
+    const double *ex, const double *ey, const double *ez,
+    const double psi, double *result, double *polarization);
+  void *ctx;
+};
+
+void blDipoleOperatorApply(struct DipoleOperator *op,
+    int stride, int numPtcls,
+    const double *ex, const double *ey, const double *ez,
+    const double psi, double *result, double *polarization);
+
+void blDipoleOperatorDestroy(struct DipoleOperator *op);
+
 #endif
 
