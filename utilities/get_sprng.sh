3b3f917a2368d4955d39c8baa35be93aa5c6ea91
Dominic Meiser <dmeiser@txcorp.com>
2016-01-09 13:23:38 -0700

    Add capability to build sprng with mpi.

--- utilities/get_sprng.sh
+++ utilities/get_sprng.sh
@@ -1,5 +1,12 @@
 #!/bin/sh
 
+MPIROOTDIR=${MPIROOTDIR:-`pwd`/mpich}
+MPICC=${MPICC:-$MPIROOTDIR/bin/mpicc}
+MPIF77=${MPIF77:-$MPIROOTDIR/bin/mpif77}
+MPILIB=${MPILIB:-"mpi"}
+MPILIBDIR=${MPILIBDIR:-$MPIROOTDIR/lib}
+MPIINCDIR=${MPIINCDIR:-$MPIROOTDIR/include}
+
 if [ -f sprng2.0/lib/libsprng.a ]; then
   echo "libsprng.a found -- nothing  to build."
 else
@@ -16,7 +23,14 @@ else
   sed -i.bak s/^PMLCGDEF/#PMLCGDEF/ make.CHOICES
   sed -i.bak s/^GMPLIB/#GMPLIB/ make.CHOICES
   sed -i.bak s/g77/echo/ SRC/make.INTEL
-  make src
+  if [ "$WITH_MPI" = "ON" ]; then
+    echo " configuring with mpi "
+    sed -i.bak s/^#MPIDEF/MPIDEF/ make.CHOICES
+    sed -i.bak '/^CFLAGS/ s/$/ -I$(MPIINCDIR) -l$(MPILIB) -L$(MPILIBDIR)/' SRC/make.INTEL
+    make MPIINCDIR=$MPIINCDIR MPILIB=$MPILIB MPILIBDIR=$MPILIBDIR src
+  else
+    make src
+  fi
   cd -
 fi
 
