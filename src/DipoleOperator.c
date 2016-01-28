54d3358b496f0cad411d152635d3d1c109ed74e7
Dominic Meiser <dmeiser@txcorp.com>
2016-01-27 21:35:13 -0700

    Simplify dipole operator application code.

--- src/DipoleOperator.c
+++ src/DipoleOperator.c
@@ -43,22 +43,20 @@ static void dipoleOperatorTLAApply(int stride, int numPtcls,
   double complex y[2];
   const double complex *x;
   double complex *r;
-  int i, j;
+  int i;
   for (i = 0; i < numPtcls; ++i) {
     x = psi + i * stride;
     r = result + i * stride;
 
-    for (j = 0; j < 2; ++j) {
-      y[j] = 0;
-    }
+    y[0] = 0;
+    y[1] = 0;
 
     double complex ep = dipoleMatrixElement * ex[i];
     y[0] = conj(ep) * x[1];
     y[1] = ep * x[0];
 
-    for (j = 0; j < 2; ++j) {
-      r[j] = y[j];
-    }
+    r[0] = y[0];
+    r[1] = y[1];
   }
 }
 
