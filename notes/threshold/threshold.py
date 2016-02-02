b779f59b7482b3597f7016e4d8f212b5b0f82ded
Dominic Meiser <dmeiser@txcorp.com>
2016-02-02 14:04:36 -0700

    Add a script to do threshold runs.

---
+++ notes/threshold/threshold.py
@@ -0,0 +1,55 @@
+import subprocess
+import numpy as np
+import os
+
+top_dir = os.getcwd()
+
+beam_laser_tla_path = top_dir + '/../../build-mpi/examples/'
+beam_laser_tla_exec = beam_laser_tla_path + 'BeamLaserTLA'
+mpi_exec = top_dir + '/../../mpich/bin/mpirun'
+
+nbar_base = 10
+vbar = 1.0e2
+dt = 1.0e-9
+dump_period = 1.0e2
+num_steps = 10001
+kappa = 1.0e8
+dipole_matrix_elemen = 1.0e-31
+ptcl_weight = 1.0e0
+
+for i in range(5):
+    run_dir = top_dir + '/run' + str(i)
+    try:
+        os.mkdir(run_dir)
+    except OSError:
+        print "Failed to create directory -- assuming it already exists."
+
+    os.chdir(run_dir)
+
+    nbar = nbar_base * 10**i
+    run_cmd = [
+            mpi_exec,
+            '-np', str(4),
+            beam_laser_tla_exec,
+            "--nbar", str(nbar),
+            "--maxNumPtcl", str(100 * nbar),
+            "--vbar", str(vbar),
+            "--kappa", str(kappa),
+            "--dt", str(dt),
+            "--numSteps", str(num_steps),
+            "--dumpPeriod", str(dump_period),
+            "--dipoleMatrixElement", str(dipole_matrix_elemen),
+            "--ptclWeight", str(ptcl_weight),
+            "--dumpPhaseSpace",
+            "--dumpField",
+            "--dumpInternalState"
+            ]
+    log_file = open('run' + str(i) + '.txt', 'w')
+    log_file.write("# Executing: \n")
+    log_file.write("# " + " ".join(run_cmd))
+    output = subprocess.Popen(run_cmd, stdout=subprocess.PIPE).communicate()[0]
+    log_file.write(output)
+    log_file.close()
+
+    os.chdir(top_dir)
+
