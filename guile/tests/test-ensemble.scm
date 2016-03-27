7edc56ba58451b6bf46f05be0bcd761b4c2c908a
Dominic Meiser <dmeiser@txcorp.com>
2016-03-27 17:39:43 -0600

    Better unit tests for guile bindings to ensemble.

--- guile/tests/test-ensemble.scm
+++ guile/tests/test-ensemble.scm
@@ -5,7 +5,33 @@
 
 (test-begin "ensemble")
 
-(test-assert "dummy test" #t)
+(test-assert "Can be created"
+  (ensemble-create "e1" 20 4))
+
+(test-assert "Set number of particles"
+  (let ((e (ensemble-create "e1" 20 4)))
+    (begin
+      (ensemble-set-num-ptcls 5 e)
+      (eq? 5 (ensemble-get-num-ptcls e)))))
+
+(test-assert "Can create space"
+  (let ((e (ensemble-create "e1" 20 4)))
+    (ensemble-create-space 5 e)))
+
+(test-assert "Can create more space than original capacity"
+  (let ((e (ensemble-create "e1" 20 4)))
+    (ensemble-create-space 25 e)))
+
+(test-assert "Get component"
+  (let ((e (ensemble-create "e1" 20 4)))
+    (begin
+      (ensemble-set-num-ptcls 4 e)
+      (let
+          ((comp (ensemble-get-component 0 e)))
+        (and
+         (eq? 4 (array-length comp))
+         (eq? 1 (array-rank comp)))
+      ))))
 
 (test-end "ensemble")
 
